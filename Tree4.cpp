80ebe17e5085322b0336a20fc5385b6a21b3270f
goba-boba <xixa3190@gmail.com>
2025-07-06 20:32:34 +0300

    Add files via upload

---
+++ Tree4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int value) : val(value), left(nullptr), right(nullptr) {}
+};
+
+int maxDiameter = 0;
+
+int height(TreeNode* root) {
+    if (!root) return 0;
+
+    int leftH = height(root->left);
+    int rightH = height(root->right);
+
+    maxDiameter = max(maxDiameter, leftH + rightH);
+
+    return 1 + max(leftH, rightH);
+}
+
+int diameterOfBinaryTree(TreeNode* root) {
+    height(root);
+    return maxDiameter;
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    TreeNode* root = new TreeNode(10);
+    root->left = new TreeNode(12);
+    root->right = new TreeNode(15);
+    root->left->left = new TreeNode(25);
+    root->left->right = new TreeNode(30);
+    root->right->right = new TreeNode(36);
+
+    cout << "Диаметр дерева: " << diameterOfBinaryTree(root) << endl;
+
+    return 0;
+}
