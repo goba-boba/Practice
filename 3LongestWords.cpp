04f6d960ad0527b155ea1ac38a4093316223efd9
goba-boba <xixa3190@gmail.com>
2025-07-08 08:25:32 +0300

    Add files via upload

---
+++ 3LongestWords.cpp
@@ -0,0 +1,36 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+bool compare(const std::string& left, const std::string& right)
+{
+    return left.length() > right.length();
+}
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<std::string> allWords;
+    std::string tmpStr;
+
+
+    std::string path = "C:/Users/arish/source/repos/Files/longestWord/karenina.txt";
+    std::ifstream MyFile(path);
+
+    while (MyFile >> tmpStr) {
+        allWords.push_back(tmpStr);
+    }
+    MyFile.close();
+
+
+    std::sort(begin(allWords), end(allWords), compare);
+
+    for (int i(0); i < 3 && i < allWords.size(); i++) {
+        std::cout << allWords[i] << " " << allWords[i].length() << "\n ";
+    }
+}
+
